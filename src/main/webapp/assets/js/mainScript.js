e3e36d2551129b08a3acfc9f26b9d37fff3fcf3f
vikash kumar verma <vkdinventor@gmail.com>
2017-11-04 16:05:03 +0530

    contact tab function implemented

--- src/main/webapp/assets/js/mainScript.js
+++ src/main/webapp/assets/js/mainScript.js
@@ -1,4 +1,4 @@
-function loadContact(){
+function loadContact(group){
     var myvar = "";
     var contactRef = firebase.database().ref('users');
     var dataSet = new Array();
@@ -6,6 +6,9 @@ function loadContact(){
         snapshot.forEach(function(childSnapshot) {
           var childData = childSnapshot.val();
           dataSet.push(childData);
+          if (group != 0  && childData.group != group){
+              return;
+          }
           console.log('contact details : '+childData.name+' '+childData.phone+' '+childData.email+' '+childData.group);
           
  myvar = myvar+ '<tr>'+
@@ -39,25 +42,37 @@ function loadContact(){
         });
         document.getElementById('progressImage').style.display = 'none';
         document.getElementById('contactTable').innerHTML = myvar;
-        var family = 0;
-        var friend = 0;
-        var other = 0;
-        dataSet.forEach(function(item){
-            if(item.group == 1){
-                family++;
-            }else if(item.group == 2){
-                friend++;
-            }else {
-                other++;
-            }
-        });
-        console.log('All contact details : '+dataSet.length+' family '+family+' friend '+friend+' other: '+other);
-        document.getElementById('allContacatBadge').innerHTML = dataSet.length;
-        document.getElementById('familyContacatBadge').innerHTML = family;
-        document.getElementById('friendContacatBadge').innerHTML = friend;
-        document.getElementById('otherContacatBadge').innerHTML = other;
+
+        if (group == 0){
+            var family = 0;
+            var friend = 0;
+            var other = 0;
+            dataSet.forEach(function(item){
+                if(item.group == 1){
+                    family++;
+                }else if(item.group == 2){
+                    friend++;
+                }else {
+                    other++;
+                }
+            });
+            console.log('All contact details : '+dataSet.length+' family '+family+' friend '+friend+' other: '+other);
+            document.getElementById('allContacatBadge').innerHTML = dataSet.length;
+            document.getElementById('familyContacatBadge').innerHTML = family;
+            document.getElementById('friendContacatBadge').innerHTML = friend;
+            document.getElementById('otherContacatBadge').innerHTML = other;
+        }
     });
 }
 
-loadContact();
+function activeSelecedItem(a) {
+   var allelement =  document.getElementById('listGrup').getElementsByTagName('a');
+    for (var v=0 ; v< allelement.length ; v++) {
+        allelement[v].className = 'list-group-item';
+
+    }
+    a.className = 'list-group-item active';
+}
+
+loadContact(0);
 
